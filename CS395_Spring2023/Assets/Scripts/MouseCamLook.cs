cb6b28244a3a51bd95807221fad29520f59c970d
tnilest <tnilest@uky.edu>
2023-01-25 14:10:01 -0500

    intermediate commit

--- CS395_Spring2023/Assets/Scripts/MouseCamLook.cs
+++ CS395_Spring2023/Assets/Scripts/MouseCamLook.cs
@@ -39,20 +39,21 @@ public class MouseCamLook : MonoBehaviour
     }
 
      private void LateUpdate() {
-        //the delta value for mouse movement
-        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
-        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
-        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+        // //the delta value for mouse movement
+        // var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        // md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        // smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        // smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
 
-        mouseLook += smoothV;
+        // mouseLook += smoothV;
 
-        Quaternion angle = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
-        if(angle.x < 0.5 && angle.x > -0.5){
-            transform.localRotation = angle;
-        }
+        // Quaternion angle = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        // if(angle.x < 0.5 && angle.x > -0.5){
+        //     transform.localRotation = angle;
+        // }
 
-        rb.rotation = Quaternion.AngleAxis(mouseLook.x, Vector3.up);
+        // rb.MoveRotation(rb.rotation * Quaternion.AngleAxis(mouseLook.x, Vector3.up));
+        this.transform.forward = player.transform.forward;
     }
 
 }
--- CS395_Spring2023/Assets/Scripts/playerController.cs
+++ CS395_Spring2023/Assets/Scripts/playerController.cs
@@ -12,6 +12,11 @@ public class playerController : MonoBehaviour
     private float straffe;
     private Rigidbody rb;
     private Vector3 curr_pos;
+    //mouseCameraLook variables
+    [SerializeField]
+    public float sensitivity = 1.0f;
+    private Vector2 mouseLook;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -31,26 +36,33 @@ public class playerController : MonoBehaviour
     }
 
     void FixedUpdate() {
-        if(Vector3.Magnitude(rb.velocity) <= max_speed){
-            float curr_speed = speed;
+        float curr_speed = speed;
 
-            //detects forward backward directional input and moves proportional to speed value.
-            translation = Input.GetAxis("Vertical") * curr_speed * Time.deltaTime;
+        //detects forward backward directional input and moves proportional to speed value.
+        translation = Input.GetAxis("Vertical") * curr_speed * Time.deltaTime;
 
-            //detects left/right keyboard input
-            straffe = Input.GetAxis("Horizontal") * curr_speed * Time.deltaTime;
+        //detects left/right keyboard input
+        straffe = Input.GetAxis("Horizontal") * curr_speed * Time.deltaTime;
 
-            curr_pos = rb.position;
-            Vector3 input = new Vector3(straffe, 0, translation);
-            input = rb.rotation * input;
+        curr_pos = rb.position;
+        Vector3 input = new Vector3(straffe, 0, translation);
+        input = rb.rotation * input;
+        rb.AddForce(input, ForceMode.Impulse); 
 
-            rb.AddForce(input, ForceMode.Impulse);
-            //rb.MovePosition(curr_pos + input);
-        }
-        elseif(Vector3.Magnitude(rb.velocity) > max_speed){
-            float reduceSpeed = 
-        }
-        
+
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        md = Vector2.Scale(md, new Vector2(sensitivity, sensitivity));
+       
+        mouseLook += md;
+
+        // Quaternion angle = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        // if(angle.x < 0.5 && angle.x > -0.5){
+        //      transform.localRotation = angle;
+        // }
+        rb.rotation = Quaternion.AngleAxis(mouseLook.x, Vector3.up);
+        //rb.MoveRotation(rb.rotation * Quaternion.AngleAxis(mouseLook.x, Vector3.up));
+
+           
     }
 
 }
