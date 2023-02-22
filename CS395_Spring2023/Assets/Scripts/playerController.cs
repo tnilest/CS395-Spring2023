7de15c9ff4652ee1b0312e366760ebd445234861
tnilest <tnilest@uky.edu>
2023-02-22 15:04:56 -0500

    cleaned up code

--- CS395_Spring2023/Assets/Scripts/playerController.cs
+++ CS395_Spring2023/Assets/Scripts/playerController.cs
@@ -5,37 +5,36 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     [SerializeField]
-    public float speed = 6.0f;
-    [SerializeField]
-    public float max_speed = 15f;
-    private float vert;
-    private float horz;
-    private Rigidbody rb;
-    private Vector3 curr_pos;
+    public float speed = 25.0f; //player's movespeed
+    private float vert; //stores keyboard input for vertical movement
+    private float horz; //storese keyboard input for horizontal movement
+    private Rigidbody rb; //player's rigidbody
+    private Vector3 curr_pos; //current position
     //mouseCameraLook variables
     [SerializeField]
-    public float sensitivity = 1.0f;
-    public float smoothing = 2.0f;
-    private Vector2 mouseLook;
-    private Vector2 smoothV;
-    new private GameObject camera;
-    private float maxLookAngle = 70.0f;
-    private float minLookAngle = -70.0f;
+    public float sensitivity = 1.0f; //camera sensitivity
+    public float smoothing = 2.0f; //value that helps smooth camera movement
+    private Vector2 mouseLook; //gets axis of mouse movement
+    private Vector2 smoothV; //smoothed verision of mouse movement
+    new private GameObject camera; //camera object
+    private float maxLookAngle = 70.0f; //limits how high player can look
+    private float minLookAngle = -70.0f; //limits how low player can look
 
     public bool canJump;
-    public float jumpHeight = 5.0f;
+    public float jumpHeight = 12.0f; //sets jump height
 
     //stores the materials for interactable items
-    public Material itemColor;
-    public Material highlightedItemColor;
+    public Material itemColor; //stores item's default material color
+    public Material highlightedItemColor; //stores item's highlighted color
 
-    public GameObject lastHighlightedObject;
+    public GameObject lastHighlightedObject; //keeps track of the previously highlighted object
 
-    public float pushPower = 0.5f;
+    public float pushPower = 100.0f; //total push/pull power that is distributed between player & object
 
     // Start is called before the first frame update
     void Start()
     {
+        //initialization of values here. also locks camera during operation
         Cursor.lockState = CursorLockMode.Locked; //used to turn off mouse cursor during execution.
         rb = gameObject.GetComponent<Rigidbody>(); // collects player's rididbody
         camera = this.transform.GetChild(0).gameObject; // collects camera object, assuming it is the first child of player
@@ -45,7 +44,7 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space") && canJump)
+        if (Input.GetKeyDown("space") && canJump) //limits jumping
         {
             rb.AddForce(new Vector3(0, jumpHeight, 0), ForceMode.Impulse);
             canJump = false;
@@ -54,12 +53,13 @@ public class playerController : MonoBehaviour
 
     void FixedUpdate()
     {
+        //begin code for push/pull objects
         Ray ray = new Ray(camera.transform.position, camera.transform.forward);
         if (Physics.SphereCast(camera.transform.position, 1.0f, camera.transform.forward, out RaycastHit hit)) 
         { 
            if(hit.collider.gameObject.tag == "Metal"){ //if spherecast hits a metal object, then highlight it and can push/pull it
-                hit.collider.gameObject.GetComponent<MeshRenderer>().material = highlightedItemColor;
-                if(hit.collider.gameObject != lastHighlightedObject){
+                hit.collider.gameObject.GetComponent<MeshRenderer>().material = highlightedItemColor; //highlight interactable object you are looking at
+                if(hit.collider.gameObject != lastHighlightedObject){ //make sure that only 1 object is highlighted at a time
                     
                     if(lastHighlightedObject != null){
                         lastHighlightedObject.GetComponent<MeshRenderer>().material = itemColor;
@@ -72,23 +72,24 @@ public class playerController : MonoBehaviour
                 float netMass = playerMass + itemMass;
 
                 if(Input.GetMouseButton(1)){ //right click pulls object
-                //add force to item proportional to itemMass, playerMass, and pushPower
+                    //add force to item proportional to itemMass, playerMass, and pushPower
                     hit.collider.gameObject.GetComponent<Rigidbody>().AddForce(-1 * pushPower * (playerMass / netMass) * (hit.collider.gameObject.transform.position - this.transform.position).normalized, ForceMode.Force);
                     rb.AddForce(-1 * pushPower * (itemMass / netMass) * (this.transform.position - hit.collider.gameObject.transform.position).normalized, ForceMode.Force);
                 }
 
                 if(Input.GetMouseButton(0)){ //left click pushes object
+                    //applies propotional forces to both object at player. 
                     hit.collider.gameObject.GetComponent<Rigidbody>().AddForce(-1 * pushPower * (playerMass / netMass) * (this.transform.position - hit.collider.gameObject.transform.position).normalized, ForceMode.Force);
                     rb.AddForce(-1 * pushPower * (itemMass / netMass) * (hit.collider.gameObject.transform.position - this.transform.position).normalized, ForceMode.Force);
-                    //hit.collider.gameObject.GetComponent<Rigidbody>().AddForce(1 * camera.transform.forward, ForceMode.Impulse);
                 }
            }
            else if (hit.collider.gameObject.tag != "Metal" && lastHighlightedObject != null){ //if not looking at a metal object, then remove previous highlight
                 lastHighlightedObject.GetComponent<MeshRenderer>().material = itemColor;
            }
         }
+        //end code for push/pull
 
-        ///////////////////////////////////////// above is new work for preliminary metal interactions
+        //begin basic movement code
         float curr_speed = speed;
 
         vert = Input.GetAxis("Vertical") * curr_speed * Time.deltaTime; //detects forward backward directional input and moves proportional to speed value.
@@ -98,7 +99,6 @@ public class playerController : MonoBehaviour
         Vector3 input = new Vector3(horz, 0.2f, vert);
         input = rb.rotation * input;
         rb.AddForce(input, ForceMode.Impulse);
-        //rb.AddForce(new Vector3(0, 4, 0), ForceMode.Acceleration);
 
         var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
         md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
@@ -107,19 +107,16 @@ public class playerController : MonoBehaviour
         mouseLook += smoothV;
 
         rb.rotation = Quaternion.AngleAxis(mouseLook.x, Vector3.up);
-
-        // print(rb.velocity.magnitude);
-        if (rb.velocity.magnitude > max_speed)
-        {
-            rb.velocity = rb.velocity.normalized * max_speed;
-        }
+        //end basic movement code
     }
 
     void LateUpdate()
     {
+        //begin camera movement
         mouseLook.y = Mathf.Clamp(mouseLook.y, minLookAngle, maxLookAngle);
         Quaternion lookAngle = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
         camera.transform.localRotation = lookAngle;
+        //end camera movement
     }
 
 }
